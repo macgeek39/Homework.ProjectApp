3b44462658d01c9f9855187c8b2b803eaaf7d65e
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-03 07:26:53 -0600

    Initialized Commit

--- HomeworkProject/Program.cs
+++ HomeworkProject/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+/*
+ * Plan and build a console application that asks a user for their name and their age. 
+ * If their name is Bob or Sue, address them as Professor. If the person is under 21,
+ * recommend they wait X years to start this class.
+ * 
+ */
+
+
